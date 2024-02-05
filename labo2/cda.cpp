02ea426023296b3f15cc70a36c763498d85a81e3
mateo <mateoruizz1299@gmail.com>
2024-02-05 20:32:07 -0300

    Hasta el ejercicio 4 completo

---
+++ labo2/cda.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+#include "cda.h"
+
+using namespace std;
+
+vector<float> dca(const float *radio){
+    float diametro = 0, circunferencia = 0, area = 0;
+    float r = *radio;
+
+    diametro = r * 2;
+    circunferencia = 2*M_PI*r;
+    area = M_PI * r * r;
+
+
+    return {diametro, circunferencia, area};
+}
---
+++ labo2/ej4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+#include "cda.h"
+
+using namespace std;
+
+
+
+
+int main(){
+
+    float r = 0;
+
+    cout<<"Ingrese el radio de un circulo."<<endl;
+    cin>>r;
+
+    const float *radio = &r;
+    vector diaCirAre = dca(radio);
+
+    cout<<"El diametro es: "<<diaCirAre[0]<<endl;
+    cout<<"La circunferencia es: "<<diaCirAre[1]<<endl;
+    cout<<"El area del circulo es: "<<diaCirAre[2]<<endl;
+
+    return 0;
+}
